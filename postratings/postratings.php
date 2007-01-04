e478739552d24ee6be5b40933b25f5d8dfcad73c
GamerZ <GamerZ@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2007-01-04 15:51:36 +0000

    Fixed Minor Bug
    git-svn-id: https://plugins.svn.wordpress.org/wp-postratings/trunk@7208 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- postratings/postratings.php
+++ postratings/postratings.php
@@ -380,7 +380,7 @@ if(!function_exists('get_ipaddress')) {
 ### Function: Place Rating In Content
 add_filter('the_content', 'place_ratings', 7);
 function place_ratings($content){
-    $content = preg_replace( "/\[ratings\]/ise", "the_ratings(false)", $content);
+    $content = preg_replace( "/\[ratings\]/ise", "the_ratings('div', false)", $content);
 	return $content;
 }
 
