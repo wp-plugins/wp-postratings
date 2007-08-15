9de0833d73a2c520c298526a51da69b0593b6e1e
GamerZ <GamerZ@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2007-08-15 14:18:42 +0000

    Fixed WP-Stats API Bug
    git-svn-id: https://plugins.svn.wordpress.org/wp-postratings/trunk@17005 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- postratings/postratings.php
+++ postratings/postratings.php
@@ -954,7 +954,7 @@ if($_GET['sortby'] == 'most_rated') {
 
 
 ### Function: Plug Into WP-Stats
-if(strpos(get_option('stats_url'), $_SERVER['REQUEST_URI']) || strpos($_SERVER['REQUEST_URI'], 'stats-options.php')) {
+if(strpos(get_option('stats_url'), $_SERVER['REQUEST_URI']) || strpos($_SERVER['REQUEST_URI'], 'stats-options.php') || strpos($_SERVER['REQUEST_URI'], 'stats/stats.php')) {
 	add_filter('wp_stats_page_admin_plugins', 'postratings_page_admin_general_stats');
 	add_filter('wp_stats_page_admin_most', 'postratings_page_admin_most_stats');
 	add_filter('wp_stats_page_plugins', 'postratings_page_general_stats');
