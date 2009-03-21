9eed14ca53ec4886995f443b323afa5927265156
GamerZ <GamerZ@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-03-21 11:01:51 +0000

    FIXED: Multiple Loops Filtered Not Cleared
    git-svn-id: https://plugins.svn.wordpress.org/wp-postratings/trunk@103979 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-postratings.php
+++ wp-postratings.php
@@ -708,6 +708,15 @@ function ratings_sorting($local_wp_query) {
 		add_filter('posts_join', 'ratings_highest_join');
 		add_filter('posts_where', 'ratings_highest_where');
 		add_filter('posts_orderby', 'ratings_highest_orderby');
+	} else {
+		remove_filter('posts_fields', 'ratings_highest_fields');
+		remove_filter('posts_join', 'ratings_highest_join');
+		remove_filter('posts_where', 'ratings_highest_where');
+		remove_filter('posts_orderby', 'ratings_highest_orderby');
+		remove_filter('posts_fields', 'ratings_most_fields');
+		remove_filter('posts_join', 'ratings_most_join');
+		remove_filter('posts_where', 'ratings_most_where');
+		remove_filter('posts_orderby', 'ratings_most_orderby');
 	}
 }
 
