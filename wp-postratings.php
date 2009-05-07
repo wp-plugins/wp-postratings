bf19e9adf7d3b11bc14fa818732341864d5dc7cd
GamerZ <GamerZ@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-05-07 03:46:59 +0000

    attr to esc_attr
    git-svn-id: https://plugins.svn.wordpress.org/wp-postratings/trunk@116073 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-postratings.php
+++ wp-postratings.php
@@ -1074,14 +1074,14 @@ function expand_ratings_template($template, $post_id, $post_ratings_data = null,
 	// Display Widget
 	function widget($args, $instance) {
 		extract($args);
-		$title = attr($instance['title']);
-		$type = attr($instance['type']);
-		$mode = attr($instance['mode']);
+		$title = esc_attr($instance['title']);
+		$type = esc_attr($instance['type']);
+		$mode = esc_attr($instance['mode']);
 		$limit = intval($instance['limit']);
 		$min_votes = intval($instance['min_votes']);
 		$chars = intval($instance['chars']);
-		$cat_ids = explode(',', attr($instance['cat_ids']));
-		$time_range = attr($instance['time_range']);
+		$cat_ids = explode(',', esc_attr($instance['cat_ids']));
+		$time_range = esc_attr($instance['time_range']);
 		echo $before_widget.$before_title.$title.$after_title;
 		echo '<ul>'."\n";
 		switch($type) {
@@ -1156,14 +1156,14 @@ function expand_ratings_template($template, $post_id, $post_ratings_data = null,
 	function form($instance) {
 		global $wpdb;
 		$instance = wp_parse_args((array) $instance, array('title' => __('Ratings', 'wp-postratings'), 'type' => 'highest_rated', 'mode' => 'both', 'limit' => 10, 'min_votes' => 0, 'chars' => 200, 'cat_ids' => '0', 'time_range' => '1 day'));
-		$title = attr($instance['title']);
-		$type = attr($instance['type']);
-		$mode = attr($instance['mode']);
+		$title = esc_attr($instance['title']);
+		$type = esc_attr($instance['type']);
+		$mode = esc_attr($instance['mode']);
 		$limit = intval($instance['limit']);
 		$min_votes = intval($instance['min_votes']);
 		$chars = intval($instance['chars']);
-		$cat_ids = attr($instance['cat_ids']);
-		$time_range = attr($instance['time_range']);
+		$cat_ids = esc_attr($instance['cat_ids']);
+		$time_range = esc_attr($instance['time_range']);
 ?>
 		<p>
 			<label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:', 'wp-postratings'); ?> <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>" /></label>
