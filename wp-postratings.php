7bcea835bad9a2c6706a210d87b60fcdfd653b2f
GamerZ <GamerZ@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-11-04 22:10:13 +0000

    Fixed Template Issue
    git-svn-id: https://plugins.svn.wordpress.org/wp-postratings/trunk@307914 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-postratings.php
+++ wp-postratings.php
@@ -739,7 +739,8 @@ if(strpos(get_option('stats_url'), $_SERVER['REQUEST_URI']) || strpos($_SERVER['
 	add_filter('wp_stats_page_plugins', 'postratings_page_general_stats');
 	add_filter('wp_stats_page_most', 'postratings_page_most_stats');
 }
-
+if ($_SERVER['PHP_SELF'] == @links_add_base_url("/", $_SERVER['HTTP_REFERER'])) 
+        return;
 
 ### Function: Add WP-PostRatings General Stats To WP-Stats Page Options
 function postratings_page_admin_general_stats($content) {
