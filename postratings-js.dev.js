947b469f09081fa9904f5d241d9fbd015b3bd16a
GamerZ <GamerZ@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-06-18 00:59:49 +0000

    Check for null
    git-svn-id: https://plugins.svn.wordpress.org/wp-postratings/trunk@559579 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- postratings-js.dev.js
+++ postratings-js.dev.js
@@ -105,7 +105,7 @@ function rate_post() {
 	post_ratings_el = jQuery('#post-ratings-' + post_id);
 	if(!is_being_rated) {
 		post_ratings_nonce = jQuery(post_ratings_el).data('nonce');
-		if(typeof post_ratings_nonce == 'undefined')
+		if(typeof post_ratings_nonce == 'undefined' || post_ratings_nonce == null)
 			post_ratings_nonce = jQuery(post_ratings_el).attr('data-nonce');
 		set_is_being_rated(true);
 		if(ratingsL10n.show_fading) {
